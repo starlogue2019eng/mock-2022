ad7bcaa35d732d7593bd1a6918fdda9e31c5ce61
starlogue2019eng <61860420+starlogue2019eng@users.noreply.github.com>
2022-04-02 09:10:44 +0800

    Add files via upload
    first commit

---
+++ assets/main.js
@@ -0,0 +1,39 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: "",
+    candidate: {
+      "cand_number": null,
+      "surname": null
+    }
+  },
+  mounted: function () { 
+    var cand_number = prompt("考生編號尾三位數字"); //ask for login detail
+    var surname = prompt("英文姓氏小寫");
+    // var cand_number = "999";
+    // var surname = "demo";
+
+    var self = this;
+    $.getJSON("./assets/list.json", function (data) { //get data from json
+      data.forEach(element => {
+        if (element.cand_number == cand_number && element.surname == surname) {
+          console.log(element);
+          self.candidate = element;
+        }
+      });
+      // if data does not match login detail, then go to 404
+      if (data.some(candidate => candidate.cand_number == cand_number && candidate.surname ==
+        surname)) {
+        document.querySelector("body").style.removeProperty("visibility");
+        return true;
+      } else {
+        window.location.href = "404/index.html";
+      }
+      // end if
+    })
+    .fail(function() {
+      window.location.href = "404/index.html";
+    });
+
+  }
+} );
